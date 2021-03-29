58f52c685fa820e83d8dc2257ea11d7a87dd3328
mjf-stu <657398135@qq.com>
2021-03-29 11:52:06 +0800

    page cate

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,41 @@
 // pages/category/category.js
+import {req} from "../../request/indexReq.js"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftMenuList: [],
+    rightContent: [],
+    current: 0
   },
-
+  Cates: [],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCategoryData()
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 获取当前页面数据的方法
    */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getCategoryData(){
+    req({
+      url: "https://api-hmugo-web.itheima.net/api/public/v1/categories",
+      method: "GET"
+    }).then((result)=>{
+      console.log(result)
+      this.Cates = result.data.message
+      this.setData({
+        leftMenuList: this.Cates.map(i=>i.cat_name),
+        rightContent: this.Cates.map(i=>i.children)
+      })
+      console.log(this.data.leftMenuList)
+      console.log(this.data.rightContent)
+    })
   }
+ 
 })
