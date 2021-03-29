3f92c6461c650550ae63a252045e2571a1e8bc72
mjf-stu <657398135@qq.com>
2021-03-29 17:21:14 +0800

    渲染页面

--- pages/category/category.js
+++ pages/category/category.js
@@ -9,14 +9,28 @@ Page({
   data: {
     leftMenuList: [],
     rightContent: [],
-    current: 0
+    current: 0,
+    scrollTop: 0
   },
   Cates: [],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getCategoryData()
+      this.getCategoryData()
+
+    // 缓存
+    // let Cates = wx.getStorageSync("Cates");
+    // console.log(Cates)
+    // if(!Cates){
+    //   this.getCategoryData()
+    // }
+    // else{
+    //   this.setData({
+    //     leftMenuList: Cates.data.map(i=>i.cat_name),
+    //     rightContent: Cates.data.map(i=>i.children)
+    //   })
+    // }
   },
 
   /**
@@ -33,9 +47,24 @@ Page({
         leftMenuList: this.Cates.map(i=>i.cat_name),
         rightContent: this.Cates.map(i=>i.children)
       })
+      // 缓存数据
+      wx.setStorageSync('Cates', {time: Date.now(),data: this.Cates});
       console.log(this.data.leftMenuList)
       console.log(this.data.rightContent)
     })
-  }
+  },
  
+  /**
+   * 事件相关方法
+   */
+  itemSelected(e){
+    let i = e.currentTarget.dataset.i
+    if(i!==this.data.current){
+      console.log(this.data.scrollTop)
+      this.setData({
+        current: i,
+        scrollTop: 0
+      })
+    }
+  }
 })
