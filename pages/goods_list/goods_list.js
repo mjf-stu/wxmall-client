26bc647e4447af87660ee2ec4619155e2f7f8b43
mjf-stu <657398135@qq.com>
2021-03-30 17:49:17 +0800

    goodsList

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -7,51 +7,70 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        name:"综合",
+        isActive: true
+      },{
+        name:"销量",
+        isActive: false
+      },{
+        name:"价格",
+        isActive: false
+      }
+    ],
+    goods: [],
+    tipsShow: 0
   },
-
+  // 列表查询需要的参数
+  queryParams:{
+    query: "",
+    cid: "0",
+    pagenum: 1,
+    pagesize: 10,
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    req({
-      url: "/goods/search",
-      method: "GET",
-      data: {
-        cid: options.catid,
-        pagenum: 1
-      }
-    }).then((result)=>{
-      console.log(result)
+    this.queryParams.cid = options.catid
+    this.getGoods(this.queryParams)
+    // tips的显示隐藏防抖函数赋值
+    this.tipsDebounce = this.debounce(()=>{
+      this.setData({
+        tipsShow: 0
+      })
+    },3000)
+  },
+
+  /**
+   *  方法  
+   */
+  // 网络请求
+  async getGoods(options){
+    const result = await req({url: "/goods/search",method: "GET",data: {...options}})
+    let goods = this.data.goods
+    if(result.goods.length === 0){
+      return 0
+    }
+    else{
+      goods.push(...result.goods)
+      this.setData({
+        goods
+      })
+    }
+    console.log(this.data.goods)
+  },
+  // 事件
+  changeTabs(e){
+    const i = e.detail.index
+    const tabs = this.data.tabs
+    tabs.forEach((item,index)=>{
+      index === i ? item.isActive = true : item.isActive = false
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+    this.setData({
+      tabs
+    })
   },
 
   /**
@@ -65,13 +84,31 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+    this.queryParams.pagenum++
+    const result = this.getGoods(this.queryParams)
+    result.then(result=>{
+      if(result===0){
+        this.queryParams.pagenum--
+        
+        this.setData({
+          tipsShow: 1
+        })
+        this.tipsDebounce()
+      }
+    })
+  },
+  tipsDebounce(){
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  debounce(fn,dely){
+    let timer = null 
+    return function(){
+      if(timer !== null){
+        clearTimeout(timer)
+      }
+      timer = setTimeout(() => {
+        fn()
+      }, dely);
+    }
   }
 })
