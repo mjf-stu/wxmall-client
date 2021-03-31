b2424c979cbcc4ec756ad06cb8395816ae9e809c
mjf-stu <657398135@qq.com>
2021-03-31 17:15:52 +0800

    detail

--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -1,66 +1,28 @@
 // pages/goods_detail/goods_detail.js
+import {req} from "../../request/indexReq.js"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsData: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getDetailData(options)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  async getDetailData(options){
+    let result = await req({url: "/goods/detail",data: {goods_id: options.goods_id},method: "GET"})
+    this.setData({
+      goodsData: result
+    })  
+    console.log(result)
   }
+
 })
