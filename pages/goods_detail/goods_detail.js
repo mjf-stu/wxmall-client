c6c642b6befdde3d4c864b837cc68862de6073c5
mjf-stu <657398135@qq.com>
2021-04-01 17:23:59 +0800

    bigImg

--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -7,7 +7,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    goodsData: {}
+    goodsData: {},
+    bigPicsShow: 0,
+    initImgIndex: 0
   },
 
   /**
@@ -20,9 +22,27 @@ Page({
   async getDetailData(options){
     let result = await req({url: "/goods/detail",data: {goods_id: options.goods_id},method: "GET"})
     this.setData({
-      goodsData: result
+      goodsData: {
+        pics:result.pics,
+        goods_price:result.goods_price,
+        goods_name:result.goods_name,
+        goods_introduce:result.goods_introduce.replace(/\.webp/g,".jpg")
+      }
     })  
     console.log(result)
-  }
+  },
 
+  showBigImage(e){
+    this.setData({
+      initImgIndex: e.currentTarget.dataset.index,
+      bigPicsShow: 1
+    })
+  },
+
+  closeBigImg(e){
+    this.setData({
+      initImgIndex: 0,
+      bigPicsShow: 0
+    })
+  }
 })
